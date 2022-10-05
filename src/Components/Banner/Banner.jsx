7ca7b7601c66015cc94a5cbfce324b96f9e0b257
Afshal1 <afshalhassan7@gmail.com>
2022-10-05 23:26:34 +0500

    Version 1.0

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,15 +6,15 @@ function Banner() {
     <>
         <div className='Banner'>
 
-            <Container style={{width:"50%",boxSizing:"border-box",display:"flex",alignItems:"center",justifyContent:"center"}}>
+            <Container style={{width:"50%",boxSizing:"border-box",display:"flex",alignItems:"center",justifyContent:"center"}} className="banner-intro">
                         <div className='tagline'>
                                 <Typography className='tagline-title' variant='h2'><span style={{fontWeight:"bold"}}>Afshal</span><span className='tagline-tile2'>Hassan</span></Typography><div className='divider'></div>
                                 <Typography variant='subtitle1' className='subtitle'>Visual Product Designer and Developer</Typography>
                         </div>
             </Container>
-            <Container style={{width:"62%",display:"flex",justifyContent:"center",alignItems:"center",border:"2px solid blue",boxSizing:"border-box",backgroundColor:"#1484CD"}} className="banner-wavy">
+            <Container style={{display:"flex",justifyContent:"center",alignItems:"center",boxSizing:"border-box",backgroundColor:"#1484CD"}} className="banner-wavy">
                   {/* <img src={Picture} alt="Picture" style={{width:265,height:265,borderRadius:"100%",marginLeft:150}} className="picture"/> */}
-                  <Card style={{width:380,height:340,boxShadow:"0 4px 30px rgba(0,0,0,0.1)",background:"rgba(255,255,255,0.2)",overflow:"hidden",borderRadius:15,border:"1px solid rgba(255,255,255,0.3)"}} className="Card">
+                  <Card style={{boxShadow:"0 4px 30px rgba(0,0,0,0.1)",background:"rgba(255,255,255,0.2)",borderRadius:15,border:"1px solid rgba(255,255,255,0.3)"}} className="Card">
                     <Typography variant='h4' className='About-Me'>About Me</Typography>
                     <Typography variant='subtitle2' className='AboutMe-Desc'>Afshal Hassan is a full-stack developer with the love of a UI/UX designer. Listed skills include all things JavaScript, such as ReactJS, React Native, HTML5, CSS3, JQuery and Ajax as well as UI/UX design through Figma and Adobe XD.It also has a solid background in JavaScript frameworks like intimately familiar with the MERN (MongoDB, Express and Node.js) as well as Java and it's frameworks like (Spring, Spring Boot, Spring Security, Spring Data and JPA) software stack.
 
@@ -22,6 +22,7 @@ function Banner() {
                   </Card>
             </Container>
         </div>
+        <div className='divider1'></div>
         </>
 
   )
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -4,9 +4,6 @@ import React from "react";
 import SkillLoader from "../SkillLoader/SkillLoader";
 import "../Skills/Skills.css";
 function Skills() {
-
-
-
   return (
     <Container className="Skills">
       <Typography
@@ -20,16 +17,77 @@ function Skills() {
       >
         Skills
       </Typography>
-     
-      <Container style={{display:"flex",alignItems:"center",marginTop:70,justifyContent:"space-around"}}>
-            <SkillLoader percent={65} offset={165}/>
-            <SkillLoader percent={75} offset={130}/>
-            <SkillLoader percent={80} offset={100}/>
+
+      <Container
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: 70,
+          justifyContent: "space-around",
+        }}
+        className="SkillLoader-spinner"
+      >
+        <Container className="Comm-Skill">
+          {" "}
+          <SkillLoader percent={65} offset={165} />{" "}
+          <Typography
+            variant="subtitle1"
+            style={{ textAlign: "center", fontWeight: "bold", flex: 1 }}
+            className='mobile'
+          >
+            Communication Skills
+          </Typography>
+        </Container>
+        <Container className="Comm-Skill">
+          <SkillLoader percent={75} offset={130} />{" "}
+          <Typography
+            variant="subtitle1"
+            style={{ textAlign: "center", fontWeight: "bold", flex: 1 }}
+            className='mobile'
+
+          >
+            Productivity
+          </Typography>
+        </Container>
+       <Container className="Comm-Skill"><SkillLoader percent={80} offset={100} /><Typography
+          variant="subtitle1"
+          style={{ textAlign: "center", fontWeight: "bold", flex: 1 }}
+          className='mobile'
+
+        
+        >
+          Designing
+        </Typography></Container>
       </Container>
-      <Container style={{display:"flex",justifyContent:"space-around",marginTop:27,textAlign:"center"}}>
-       <Typography variant="subtitle1" style={{textAlign:"center",fontWeight:"bold",flex:1}}>Communication Skills</Typography>
-       <Typography variant="subtitle1" style={{textAlign:"center",fontWeight:"bold",flex:1}}>Productivity</Typography>
-       <Typography variant="subtitle1" style={{textAlign:"center",fontWeight:"bold",flex:1}}>Designing</Typography>
+      <Container
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          marginTop: 27,
+          textAlign: "center",
+        }}
+      >
+        <Typography
+          variant="subtitle1"
+          style={{ textAlign: "center", fontWeight: "bold", flex: 1 }}
+          className="desktop"
+        >
+          Communication Skills
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ textAlign: "center", fontWeight: "bold", flex: 1 }}
+          className="desktop"
+        >
+          Productivity
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ textAlign: "center", fontWeight: "bold", flex: 1 }}
+          className="desktop"
+        >
+          Designing
+        </Typography>
       </Container>
     </Container>
   );
